7cdef49dc59af664e567adb5c941bd61ff1afab6
ToukaSama <blacksummoner6659@gmail.com>
2025-05-08 17:13:19 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -36,7 +36,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Usern
 #from bot.helper_funcs.utils import(
 #  delete_downloads
 #)
-os.system("wget https://graph.org/file/a96a55843e8ecb6166034.png -O thumb.jpg")
+os.system("wget https://graph.org/file/33d233807b0af9f8bc61f.png -O thumb.jpg")
 
 #LOGS_CHANNEL = -1001283278354
 CURRENT_PROCESSES = {}
